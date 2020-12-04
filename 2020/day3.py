58fec3edc195ebb6f928d464c184b5c4d7d3094e
Nathan Dileas <ndileas@gmail.com>
2020-12-03 20:29:52 -0500

    Fix strings

--- 2020/day3.py
+++ 2020/day3.py
@@ -21,9 +21,9 @@ with open('input3.txt', 'r') as f:
 	input3lines = f.read().splitlines()
 
 print('Part 1 Test:', check_slope((3,1), test3lines), '==', 7)
-print('Part 1 Test:', check_slope((3,1), input3lines), '==', 250)
+print('Part 1 Answer:', check_slope((3,1), input3lines), '==', 250)
 
 p2t = [check_slope(slope, test3lines) for slope in ((1,1), (3,1), (5,1), (7,1), (1,2))]
-print('Part 2 test: ', p2t, reduce(lambda a,b:a*b, p2t), '== [2, 7, 3, 4, 2], 366')
+print('Part 2 Test: ', p2t, reduce(lambda a,b:a*b, p2t), '== [2, 7, 3, 4, 2], 366')
 p2 = [check_slope(slope, input3lines) for slope in ((1,1), (3,1), (5,1), (7,1), (1,2))]
-print('Part 2 test: ', reduce(lambda a,b:a*b, p2), '== 1592662500')
+print('Part 2 Answer: ', reduce(lambda a,b:a*b, p2), '== 1592662500')
